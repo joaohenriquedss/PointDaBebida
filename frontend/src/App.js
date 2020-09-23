b4c1ce71273160dac1880497e3e9a7a4220c11de
joaohenriquedss <jhdss8194@gmail.com>
2020-09-23 20:23:07 -0300

    criando rota

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Login from './pages/Login'
-
+import Routes from './routes'
 import './App.css';
 
 function App() {
   return (
-    <Login />
+    <Routes />
   );
 }
 
