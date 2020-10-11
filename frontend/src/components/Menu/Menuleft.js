7d3c572eeef74c2d6c1e34f254131bac039eba3e
joaohenriquedss <jhdss8194@gmail.com>
2020-10-11 20:42:46 -0300

    criando get e set produtos

--- frontend/src/components/Menu/Menuleft.js
+++ frontend/src/components/Menu/Menuleft.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Menuleft.css';
-
+import home from '../../imgs/home.png'
 
 export default function Menuleft(props) {
   return (
     <div className='menuleft-container'>
       <div className='menuleft-content'>
         <div className="sidenav">
-          <a href="/main">Inicio</a>
+          <div>
+            
+            <a href="/main" > Inicio</a>
+          </div>
           <a href="/category">Categoria</a>
           <a href="/product">Produto</a>
         </div>
